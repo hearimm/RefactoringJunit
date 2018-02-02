822db95a1b426bf157dce20b1e2553dacc650ced
Hulk Choi <doksul23@gmail.com>
2018-02-02 18:31:57 +0900

    emptyRead용 픽스처를 추가로 작성

--- src/test/java/FileReaderTester.java
+++ src/test/java/FileReaderTester.java
@@ -1,16 +1,14 @@
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class FileReaderTester extends TestCase {
 
   private FileReader _input;
+  private FileReader _empty;
 
   public FileReaderTester(String name) {
     super(name);
@@ -21,10 +19,18 @@ public class FileReaderTester extends TestCase {
       // 본인 폴더 경로로 넣어주세용
       _input = new FileReader(
           "/Users/Hyuk/Documents/devWork/RefactoringJunit/src/main/resources/data.txt");
+      _empty = newEmptyFile();
 
-    } catch (FileNotFoundException e) {
+    } catch (IOException e) {
       throw new RuntimeException("테스트 파일을 열 수 없음");
     }
+  }
+
+  private FileReader newEmptyFile() throws IOException {
+    File empty = new File("empty.txt");
+    FileOutputStream out = new FileOutputStream(empty);
+    out.close();
+    return new FileReader(empty);
   }
 
   protected void tearDown() {
@@ -62,11 +68,7 @@ public class FileReaderTester extends TestCase {
   }
 
   public void testEmptyRead() throws IOException {
-    File empty = new File("empty.txt");
-    FileOutputStream out = new FileOutputStream(empty);
-    out.close();
-    FileReader in = new FileReader(empty);
-    assertEquals(-1,in.read());
+    assertEquals(-1,_empty.read());
   }
 
   public static void main(String[] args) {
